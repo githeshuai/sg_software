839cce67df45091deac10de8f8d9194bbf5c91f9
Matt Daw <matt@mattdaw.com>
2013-02-13 11:54:14 -0800

    Early steps on the Mac

---
+++ ShotgunIntegration/DialogManager.h
@@ -0,0 +1,25 @@
+#ifndef DialogManager_h__
+#define DialogManager_h__
+
+#include <vector>
+#include <string>
+#include <boost/noncopyable.hpp>
+#include <boost/function.hpp>
+#include "BrowserHost.h"
+ 
+typedef boost::function<void (const FB::VariantList&)> PathCallback;
+ 
+namespace FB { class PluginWindow; }
+ 
+class DialogManager
+{
+public:
+    static DialogManager* get();
+    virtual void OpenFolderDialog(const FB::BrowserHostPtr& host, FB::PluginWindow* win, const PathCallback& cb) = 0;
+ 
+protected:
+    DialogManager() {}
+    virtual ~DialogManager() {}
+};
+ 
+#endif // DialogManager_h__
---
+++ ShotgunIntegration/Mac/DialogManagerMac.mm
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <boost/thread.hpp>
+#include <AppKit/AppKit.h>
+#include <Cocoa/Cocoa.h>
+#include "logging.h"
+ 
+#include "DialogManagerMac.h"
+#include "BrowserHost.h"
+ 
+DialogManager* DialogManager::get()
+{
+    static DialogManagerMac inst;
+    return &inst;
+}
+ 
+ 
+void DialogManagerMac::OpenFolderDialog(const FB::BrowserHostPtr& host, FB::PluginWindow* win, const PathCallback& cb)
+{
+    host->ScheduleOnMainThread(boost::shared_ptr<DialogManagerMac>(), boost::bind(&DialogManagerMac::_showFolderDialog, this, win, cb));
+}
+ 
+void DialogManagerMac::_showFolderDialog(FB::PluginWindow* win, const PathCallback& cb)
+{
+    FB::VariantList out;
+    int result;
+    NSAutoreleasePool* pool = [NSAutoreleasePool new];
+    NSOpenPanel *oPanel = [NSOpenPanel openPanel];
+    
+    [oPanel setAllowsMultipleSelection:YES];
+    [oPanel setCanChooseFiles:YES];
+    [oPanel setCanChooseDirectories:YES];
+    result = [oPanel runModalForDirectory:nil file:nil types:nil];
+    
+    if (result == NSOKButton) {
+        NSArray *filesToOpen = [oPanel filenames];
+        
+        for ( NSString *path in filesToOpen ) {
+            out.push_back([path cStringUsingEncoding:[NSString defaultCStringEncoding]]);
+        }
+    }
+    
+    [pool release];
+    
+    cb(out);
+}
