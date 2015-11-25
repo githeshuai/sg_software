10f49e66e6acc8f90c1d7b0845a38a5caaa56eee
heshuai <276575758@qq.com>
2015-11-25 10:51:17 +0800

    hello

--- ShotgunIntegration/WildcardMatch.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2013 Shotgun Software Inc.
-
-#ifndef WildcardMatch_h__
-#define WildcardMatch_h__
-
-bool WildcardMatch(const std::string &pattern, const std::string &str);
-
-#endif // WildcardMatch_h__
